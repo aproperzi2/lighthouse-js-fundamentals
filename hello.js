caaf7967f8e5b825155cc7e15ae6b84f8f6ae82a
Luca Properzi <abramoproperzi@gmail.com>
2020-10-05 01:51:37 +0000

    Simple say hello function

---
+++ hello.js
@@ -0,0 +1,16 @@
+// const sayHello = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+
+
+const sayHello = function (name) {
+  console.log(`Hello, ${name}`);
+}
+
+sayHello("Luca");
+sayHello("Mark");
+sayHello("Mitch");
