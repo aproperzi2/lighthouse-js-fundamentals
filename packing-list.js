1c600f24a3d7466796588b06b71f0ec8cba4eb27
Luca Properzi <abramoproperzi@gmail.com>
2020-10-05 18:07:29 +0000

    using while loop to iterate through array

--- packing-list.js
+++ packing-list.js
@@ -3,7 +3,14 @@ const packingList = ['bowls', 'plates', 'pots', 'pans', 'eating utensils', 'glas
 console.log('Kitchen stuff to pack:');
 
 // setting a counter variable makes it east to iterate through an array 
-for (let i = 0; i < packingList.length; i++) {
+  // for (let i = 0; i < packingList.length; i++) {
+  //   console.log(packingList[i]);
+    // }
+
+
+// using while loop to iterate through array
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
-  
+  i++;
 }
