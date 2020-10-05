94767a12d3a1bd08b46c5d63f4b876f93b5de33b
Luca Properzi <abramoproperzi@gmail.com>
2020-10-05 18:04:11 +0000

    looping through array

---
+++ packing-list.js
@@ -0,0 +1,9 @@
+const packingList = ['bowls', 'plates', 'pots', 'pans', 'eating utensils', 'glasses', 'cups', 'cooking utensils'];
+
+console.log('Kitchen stuff to pack:');
+
+// setting a counter variable makes it east to iterate through an array 
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+  
+}
